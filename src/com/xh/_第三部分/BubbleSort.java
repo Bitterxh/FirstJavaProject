40ff8fedb19f9104b5014f41344f3f8be0ca669e
xh <xl6666660213.163.com>
2023-03-11 22:15:13 +0800

    Test2

---
+++ src/com/xh/_第三部分/BubbleSort.java
@@ -0,0 +1,21 @@
+package com.xh._第三部分;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {21, 33, 12, 88, -9};
+        //双重循环，外循环:n-1，内循环控制比较次数（逐渐减少）
+        for (int i = 0; i < arr.length-1; i++){
+            for (int j = 0; j < arr.length-i-1; j++){
+                if (arr[j] > arr[j+1]) {
+                    //设置一个中间变量，来实现互换，达到冒泡的效果
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
